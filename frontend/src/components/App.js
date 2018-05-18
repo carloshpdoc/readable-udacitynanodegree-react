763c95b14c77419ed7c04e6d0ac68b93d4c9cd38
Carlos Henrique <carloshenrique.pdoc@gmail.com>
2018-05-18 08:44:49 -0300

    first commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Button variant="raised" color="primary">
+      Hello World
+    </Button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
